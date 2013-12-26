3d94de251402de352ec92544920fc91782ab25fc
yu000hong <yu000hong@sina.com>
2013-12-26 17:35:40 +0800

    Add notification before locking screen.

--- home/screen-lock.sh
+++ home/screen-lock.sh
@@ -4,4 +4,21 @@ source ~/.DBUS_temp
 
 export DBUS_SESSION_BUS_ADDRESS
 
+
+# You need to install package "libnotify-bin",
+# in order to use notify-send.
+# 
+# sudo apt-get install libnotify-bin
+#
+# send notification before locking the screen.
+notify-send 锁屏警告 "<font size=6 color=red><b>系统将在一分钟后锁屏，请休息一会！</b></font>"
+
+# sleep 60 seconds
+sleep 60
+
+# You need to install package "gnome-screensaver",
+# in order to use gnome-screensaver-command.
+#
+# sudo apt-get install gnome-screensaver
+#
 /usr/bin/gnome-screensaver-command -l
