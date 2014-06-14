0c757748d0be86d41f3d04ccdd136cb0024632dc
yu000hong <yu000hong@sina.com>
2014-06-14 23:57:56 +0800

    modify chrome installation

--- install/chrome.sh
+++ install/chrome.sh
@@ -1,17 +1,54 @@
 #!/bin/bash
 
+dir=`dirname $0`
+dir=`cd $dir > /dev/null ; pwd`
+source $dir/common.sh
+
 INSTALL_DIR=${INSTALL_DIR:-/opt}
 WORK_DIR=${WORK_DIR:-~/install}
 
 # Constants
+SOFTWARE="Google Chrome"
 URL="https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb"
-FILENAME="google-chrome-stable_current_i386.deb"
+FILENAME_REGEX="google-chrome*.deb"
 
+[ -d $WORK_DIR ] || mkdir $WORK_DIR
 cd $WORK_DIR
-wget $URL
-if [ ! $? -eq 0 ]; then
-    echo "Fail to download chrome deb package."
-    exit -1
+
+package_exists $WORK_DIR "$FILENAME_REGEX" f
+if [ $? -eq 1 ] ; then 
+    wget -c $URL || {
+        fail_install $SOFTWARE "Cannot download package $URL"
+        exit 1
+    }
 fi
 
-sudo dpkg -i $FILENAME
+files=( `get_package_name $WORK_DIR "$FILENAME_REGEX" f` )
+if [ ${#files[@]} -gt 1 ] ; then
+    echo -e "\e[1;34m$SOFTWARE package selection:\e[0m"
+    select packageName in ${files[@]}
+    do
+        if [ "$packageName" != "" ] ; then
+            break
+        else
+            echo "Please choose the right number."
+        fi
+    done
+elif [ ${#files[@]} -eq 1 ] ; then
+    packageName=${files[0]}
+else
+    fail_install $SOFTWARE "No matching package"
+    exit 1
+fi
+
+##############################################
+#.deb package
+sudo dpkg -i $packageName
+if [ $? -eq 0 ] ; then
+    success_install $SOFTWARE
+    exit 0
+else
+    fail_install $SOFTWARE
+    exit 1
+fi
+
